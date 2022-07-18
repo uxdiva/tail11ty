99c28e9c33c46691ca7e2de32b57d36d86ece4c6
uxdiva <uxdiva@darasplace.com>
2022-07-18 02:31:29 +0000

    installed 11ty with tailwind

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const htmlmin = require('html-minifier')
+const now = String(Date.now())
+
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
+  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+      return minified
+    }
+
+    return content
+  })
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tail11ty
+
+see [See CSS Tricks](https://css-tricks.com/eleventy-starter-with-tailwind-css-alpine-js/)
+
+To research more:
+- Markdown-it attributes for using class, id on elements
+- @import css
+
+Pug does work for adding classes... so that's an option instead of having to do html or nunjucks.
